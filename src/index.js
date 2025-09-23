2c9a88e30a2550125e9a3c3af646b47e8eab00c8
mohit <2003mohitkhemani@gmail.com>
2025-09-23 15:16:13 +0530

    express setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import { config } from "dotenv";
 import connectDB from "./db/db.js";
-
+import app from "./app.js";
 config()
 
 connectDB()
-console.log("working",process.env.PORT);
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port`, process.env.PORT);
+        })
+    })
+    .catch(err => {
+        console.log("connection error", err);
+    })
